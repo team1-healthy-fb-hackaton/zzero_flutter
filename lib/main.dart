014ac7070c4f8c33298285ba88daa4ca3dfd8f50
jymin99 <73513813+jymin99@users.noreply.github.com>
2024-05-04 01:07:20 +0900

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+import 'home_page/main_page.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
