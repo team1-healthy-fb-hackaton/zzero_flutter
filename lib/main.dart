5f35bc31260960f334c409dba3082b53af369d38
jymin99 <73513813+jymin99@users.noreply.github.com>
2024-05-04 05:51:47 +0900

    ReviewWrite페이지 이미지, string 설정

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       color: Colors.white,
-      home: MainPage(),
+      home: ReviewWrite(),
     );
   }
 }
